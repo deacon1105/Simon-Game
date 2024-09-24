f7d2ea7847a8b14a02bf070a1b991e19122062bf
Sujal Bhaedwaj <168054817+deacon1105@users.noreply.github.com>
2024-09-24 19:09:10 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,75 @@
+var buttonColours = ["red", "blue", "green", "yellow"]; // Button colors
+var gamePattern = []; // Empty array for the game pattern
+var userClickedPattern = []; // Empty array for user clicks
+var started = false; // Game state
+var level = 0; // Game level
+
+// Start game on keypress or touchstart on mobile
+$(document).on("keypress touchstart", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// Detect clicks and touches on buttons
+$(".btn").on("click touchstart", function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("LEVEL " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
